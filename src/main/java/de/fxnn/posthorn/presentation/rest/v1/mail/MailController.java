d4a6f405b64728fac1519615b0d0078ca816d9c3
Felix <dev@fxnn.de>
2014-10-25 16:12:18 +0200

    minor code improvements

--- src/main/java/de/fxnn/posthorn/presentation/rest/v1/mail/MailController.java
+++ src/main/java/de/fxnn/posthorn/presentation/rest/v1/mail/MailController.java
@@ -1,6 +1,6 @@
 package de.fxnn.posthorn.presentation.rest.v1.mail;
 
-import static org.springframework.hateoas.mvc.ControllerLinkBuilder.*;
+import java.net.URI;
 
 import de.fxnn.posthorn.business.mail.entity.Mail;
 import org.apache.commons.lang.builder.ReflectionToStringBuilder;
@@ -12,22 +12,30 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
+
 @Controller
 @RequestMapping("/v1/mail")
 public class MailController {
-    
-    @RequestMapping(value = "/id/{mailId}", method = RequestMethod.GET)
-    public ResponseEntity<Mail> getMetaData(@PathVariable String mailId) {
-        final Mail mail = new Mail(mailId, "from@example.com", "to1@example.com", "to2@example.com");
-        mail.add(linkTo(methodOn(MailController.class).getMetaData(mailId)).withSelfRel());
-        
-        return ResponseEntity.ok(mail);
-    }
+
+  @RequestMapping(value = "/id/{mailId}", method = RequestMethod.GET)
+  public ResponseEntity<Mail> getMetaData(@PathVariable String mailId) {
+    final Mail mail = new Mail(mailId, "from@example.com", "to1@example.com", "to2@example.com");
+    mail.add(linkTo(methodOn(MailController.class).getMetaData(mailId)).withSelfRel());
+
+    return ResponseEntity.ok(mail);
+  }
 
   @RequestMapping(value = "/create", method = RequestMethod.POST)
   public ResponseEntity<Void> create(@RequestBody Mail mail) {
     System.out.println(ReflectionToStringBuilder.reflectionToString(mail, ToStringStyle.MULTI_LINE_STYLE).toString()); // FIXME Remove System.out
-    return ResponseEntity.created(linkTo(methodOn(MailController.class).getMetaData(mail.getMailId())).toUri()).build();
+
+    return ResponseEntity.created(uriTo(mail)).build();
   }
-    
+
+  protected static URI uriTo(Mail mail) {
+    return linkTo(methodOn(MailController.class).getMetaData(mail.getMailId())).toUri();
+  }
+
 }
